0466b2634083d7b7ed956f070a74693dabc08862
hqbcn <hqbcn@outloo.com>
2026-06-28 21:33:09 +0800

    持久化properties和chat-pdf.json

--- src/main/java/com/hbb/ai/HeimaAiApplication.java
+++ src/main/java/com/hbb/ai/HeimaAiApplication.java
@@ -1,12 +1,12 @@
 package com.hbb.ai;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.ai.vectorstore.redis.autoconfigure.RedisVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(exclude =
-        RedisVectorStoreAutoConfiguration.class)
+//
+//@SpringBootApplication(exclude =
+//        RedisVectorStoreAutoConfiguration.class)
+@SpringBootApplication
 @MapperScan("com.hbb.ai.mapper")
 public class HeimaAiApplication {
 
